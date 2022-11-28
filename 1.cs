9c6acb5e45584f01049818fb73fd28d0b5f6a88c
Andrii Banduliak <banduliak1303@gmail.com>
2022-11-28 23:00:05 +0300

    hw # 6 is done

---
+++ 1.cs
@@ -0,0 +1,38 @@
+﻿using static System.Console;
+Clear();
+
+WriteLine("How many numbers do you want to enter ?");
+int length=int.Parse(ReadLine());
+int[]digits=getArr(length);
+printArr(digits);
+printCount(digits);
+
+
+void printCount(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    Write($" ->{count}");
+}
+void printArr(int[]arr)
+{
+    for (int i=0; i<digits.Length; i++)
+    {
+        Write(digits[i]);
+        if (i < digits.Length - 1) Write(", ");
+    }
+    
+}
+int[] getArr(int length)
+{
+    int[] arr=new int[length];
+    for (int i=0; i<length; i++)
+    {
+        WriteLine($"Number  {i+1}");
+        arr[i] = int.Parse(ReadLine());
+    }
+    return arr;
+}
---
+++ 2.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+Clear();
+
+Write("Enter coordinate b1= ");
+double b1 = int.Parse(ReadLine());
+Write("Enter coordinate k1= ");
+double k1 = int.Parse(ReadLine());
+Write("Enter coordinate b2= ");
+double b2 = int.Parse(ReadLine());
+Write("Enter coordinate k2= ");
+double k2 = int.Parse(ReadLine());
+
+
+
+if (k1 == k2)
+{
+    if (b1 == b2) WriteLine("Straight lines are matched");
+    else WriteLine("Lines are parallel");
+}
+else WriteLine($"The Point of intersection is at the ({x(b1, k1, b2, k2)};{y(b1, k1, x(b1, k1, b2, k2))})");
+
+
+
+double x(double b1, double k1, double b2, double k2)
+{
+    return (b2 - b1) / (k1 - k2);
+}
+double y(double b, double k, double x)
+{
+    return k * x + b;
+}
